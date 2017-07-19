8995998d1b027c0a0d0ac2aebae4988c480dd299
Prateek Saxena <prtksxna@gmail.com>
2017-07-19 08:31:02 +0530

    refactor: remove microformats as a dependency

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
-var mf = require('microformat-node');
 var validUrl = require('valid-url');
 
-var getType = ( html, callback ) => {
-  var o = { html };
-  mf.get( o, ( err,  data ) => {
-    var p = data.items[ 0 ].properties;
+var getType = ( mf, callback ) => {
+  var p = mf.items[ 0 ].properties;
 
-    // RSVP
-    if (
-      Object.keys( p ).includes( 'rsvp' ) &&
-      (
-        p.rsvp.includes( 'yes' ) ||
-        p.rsvp.includes( 'no' ) ||
-        p.rsvp.includes( 'maybe' ) ||
-        p.rsvp.includes( 'interested' )
-      )
-    ) {
-      callback( 'rsvp' );
-      return;
-    }
+  // RSVP
+  if (
+    Object.keys( p ).includes( 'rsvp' ) &&
+    (
+      p.rsvp.includes( 'yes' ) ||
+      p.rsvp.includes( 'no' ) ||
+      p.rsvp.includes( 'maybe' ) ||
+      p.rsvp.includes( 'interested' )
+    )
+  ) {
+    callback( 'rsvp' );
+    return;
+  }
 
-    // Reply
-    if (
-      Object.keys( p ).includes( 'in-reply-to' ) &&
-      validUrl.isUri( p[ 'in-reply-to' ][ 0 ] )
-    ) {
-      callback( 'reply' );
-      return;
-    }
+  // Reply
+  if (
+    Object.keys( p ).includes( 'in-reply-to' ) &&
+    validUrl.isUri( p[ 'in-reply-to' ][ 0 ] )
+  ) {
+    callback( 'reply' );
+    return;
+  }
 
-    callback( 'note' );
-  } );
+  callback( 'note' );
 };
 
 module.exports = getType;
--- tests.js
+++ tests.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var mf = require('microformat-node');
 var getType = require('./index.js');
 var fs = require( 'fs' );
 
@@ -12,9 +13,11 @@ Object.keys( types ).forEach( ( type ) => {
     types[ type ].forEach( ( file ) => {
       it( 'should correctly categorize ' + file, ( done ) => {
         fs.readFile( __dirname + '/tests/' + file + '.html', 'utf-8', ( e ,d ) => {
-          getType( d, ( t ) => {
-            assert.equal( t, type );
-            done();
+          mf.get( { html: d }, ( error, data ) => {
+            getType( data, ( t ) => {
+              assert.equal( t, type );
+              done();
+            } );
           } );
         } );
       } );
