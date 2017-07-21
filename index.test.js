af69cd5d094b466faacba7cb963db3095e865ac4
Prateek Saxena <prtksxna@gmail.com>
2017-07-21 23:59:21 +0530

    chore: get tests from suite

--- index.test.js
+++ index.test.js
@@ -1,20 +1,22 @@
 var fs = require( 'fs' );
 var expect = require( 'expect' );
 var getType = require('./index.js');
+var testsDir = './node_modules/post-type-discovery-tests/';
 
 var tests = {};
 
 // Find all the tests
-fs.readdirSync( './tests/' ).forEach( ( file ) => {
+fs.readdirSync( testsDir ).forEach( ( file ) => {
   if (
     file.indexOf( '.' ) !== 0 &&
+    file.indexOf( '-' ) !== -1 &&
     file.indexOf( 'json' ) !== -1
   ) {
     var type = file.split( '-' )[ 0 ];
     var name = file.split( '-' )[ 1 ].split( '.' )[ 0 ];
 
     tests[ type ] = tests[ type ] || {};
-    tests[ type ][ name ] = JSON.parse( fs.readFileSync( './tests/' + file, 'utf-8' ) );
+    tests[ type ][ name ] = JSON.parse( fs.readFileSync( testsDir + file, 'utf-8' ) );
   }
 } );
 
